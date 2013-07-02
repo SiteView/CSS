e475e7fa75549d245a0bcb397f491c67f3b6a7c8
xinnanzhang <xinnan.zhang@dragonflow.com>
2013-07-02 17:26:10 +0800

    修改

---
+++ com.siteview.css.topo.wizard/src/com/siteview/css/topo/wizard/scan/ScanDialog.java
@@ -0,0 +1,20 @@
+package com.xinnan.patientims.dialogs;
+
+import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.widgets.Shell;
+
+/**
+ * 点击扫描时打开dialog
+ * @author zhangxinnan
+ *
+ */
+public class ScanDialog extends TitleAreaDialog{
+
+	public ScanDialog(Shell parentShell) {
+		super(parentShell);
+		WizardDialog wizardDialog = new WizardDialog(parentShell, new SomeWizard());
+		wizardDialog.open();
+	}
+
+}
