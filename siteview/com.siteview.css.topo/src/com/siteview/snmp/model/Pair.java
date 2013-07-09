d6593e8e15530b51306267db651ba57d86c8655d
haiming.wang <haiming.wang@haimingwang-PC>
2013-07-09 16:17:06 +0800

    添加注释

--- siteview/com.siteview.css.topo/src/com/siteview/snmp/model/Pair.java
+++ siteview/com.siteview.css.topo/src/com/siteview/snmp/model/Pair.java
@@ -1,5 +1,13 @@
 package com.siteview.snmp.model;
 
+/**
+ * 一个简单的键值对的数据结构
+ * 开放getFirst getSecond两个方法获取第一个，和第二个数据。
+ * @author haiming.wang
+ *
+ * @param <K>
+ * @param <V>
+ */
 public class Pair<K,V> {
 	
 	private K key;
