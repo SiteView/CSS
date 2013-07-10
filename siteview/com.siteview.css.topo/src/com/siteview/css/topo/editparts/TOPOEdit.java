5e6084594243279131eab6c5d6660b9eaf0ed298
xinnanzhang <xinnan.zhang@dragonflow.com>
2013-07-10 15:19:40 +0800

    modify and add

--- siteview/com.siteview.css.topo/src/com/siteview/css/topo/editparts/TOPOEdit.java
+++ siteview/com.siteview.css.topo/src/com/siteview/css/topo/editparts/TOPOEdit.java
@@ -19,21 +19,28 @@ import org.eclipse.core.filesystem.URIUtil;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.draw2d.graph.Node;
-import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorSite;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.ide.FileStoreEditorInput;
 import org.eclipse.ui.part.FileEditorInput;
-
-import com.siteview.css.topo.models.TopologyModel;
 import com.siteview.snmp.common.ScanParam;
 import com.siteview.snmp.pojo.DevicePro;
 
 public class TOPOEdit extends OPIEditor {
+	/**坐标定位*/
+	final String TOP_LEFT = "TOP_LEFT";
+	final String TOP = "TOP";
+	final String TOP_RIGHT = "TOP_RIGHT";
+	final String LEFT ="LEFT";
+	final String RIGHT="RIGHT";
+	final String BOTTOM_LEFT="BOTTOM_LEFT";
+	final String BOTTOM ="BOTTOM";
+	final String BOTTOM_RIGHT="BOTTOM_RIGHT";
+	
 	/** 模型节点 */
-	TopologyModel[] model = new TopologyModel[13];
+	ImageModel[] model = new ImageModel[13];
 	/** 连接模型 */
 	ConnectionModel[] cModels = new ConnectionModel[model.length];
 	/** 测试数据 */
@@ -43,7 +50,7 @@ public class TOPOEdit extends OPIEditor {
 			"jpeg", "gif", "bmp", "png" };
 	int nnodes;
 	// List list = new A
-	private StringBuffer strContext = new StringBuffer("");
+	private StringBuffer strGml = new StringBuffer("");
 	public static final String ID = "com.siteview.css.topo.editparts.TOPOEdit";
 	static Random rand = new Random(90);
 
@@ -89,23 +96,25 @@ public class TOPOEdit extends OPIEditor {
 		int oneConnectModel = 1;
 		for (int i = 0; i < model.length; i++) {
 			// 创建 i个模型
-			model[i] = new TopologyModel();
+			model[i] = new ImageModel();
 			// 创建i个连接线模型
 			cModels[i] = new ConnectionModel(displayModel);
 			// 设置模型坐标
 			getDisplayModel().addChild(model[i]);
 		}
-		TopoParsing topParsing = new TopoParsing();
-		Map<String, String> map = topParsing.readFileByChars();
+		// 解析gml获取模型坐标,设置连接坐标
+		ReadAndCreate red = new ReadAndCreate();
+		Map<String, String> map = red.readNode();
 		Iterator its = map.keySet().iterator();
 		while (its.hasNext()) {
 			String key = (String) its.next();
 			String value = map.get(key);
 			String[] values = value.split("-");
-			String value1[] = values[0].split("[.]");
-			String value2[] = values[1].split("[.]");
-			model[Integer.parseInt(key)].setX(Integer.parseInt(value1[0]));
-			model[Integer.parseInt(key)].setY(Integer.parseInt(value2[0]));
+			// String value1[] = values[0].split("[.]");
+			// String value2[] = values[1].split("[.]");
+			System.out.println("模型id="+key+"\n"+"模型X值="+values[0]+"    模型Y值="+values[1]);
+			model[Integer.parseInt(key)].setX(Integer.parseInt(values[0]));
+			model[Integer.parseInt(key)].setY(Integer.parseInt(values[1]));
 			// IPath path = model[0].getFilename();
 			if (Integer.parseInt(key) == 0) {
 				model[0].addProperty(new FilePathProperty(PROP_IMAGE_FILE,
@@ -120,6 +129,33 @@ public class TOPOEdit extends OPIEditor {
 						"Image File", WidgetPropertyCategory.Basic, new Path(
 								"bmp_PC_Gray.bmp"), FILE_EXTENSIONS));
 				model[3].addProperty(new FilePathProperty(PROP_IMAGE_FILE,
+						"Image File", WidgetPropertyCategory.Basic, new Path(
+								"bmp_PC_Gray.bmp"), FILE_EXTENSIONS));
+				model[4].addProperty(new FilePathProperty(PROP_IMAGE_FILE,
+						"Image File", WidgetPropertyCategory.Basic, new Path(
+								"bmp_PC_Gray.bmp"), FILE_EXTENSIONS));
+				model[5].addProperty(new FilePathProperty(PROP_IMAGE_FILE,
+						"Image File", WidgetPropertyCategory.Basic, new Path(
+								"bmp_PC_Gray.bmp"), FILE_EXTENSIONS));
+				model[6].addProperty(new FilePathProperty(PROP_IMAGE_FILE,
+						"Image File", WidgetPropertyCategory.Basic, new Path(
+								"bmp_PC_Gray.bmp"), FILE_EXTENSIONS));
+				model[7].addProperty(new FilePathProperty(PROP_IMAGE_FILE,
+						"Image File", WidgetPropertyCategory.Basic, new Path(
+								"bmp_PC_Gray.bmp"), FILE_EXTENSIONS));
+				model[8].addProperty(new FilePathProperty(PROP_IMAGE_FILE,
+						"Image File", WidgetPropertyCategory.Basic, new Path(
+								"bmp_PC_Gray.bmp"), FILE_EXTENSIONS));
+				model[9].addProperty(new FilePathProperty(PROP_IMAGE_FILE,
+						"Image File", WidgetPropertyCategory.Basic, new Path(
+								"bmp_PC_Gray.bmp"), FILE_EXTENSIONS));
+				model[10].addProperty(new FilePathProperty(PROP_IMAGE_FILE,
+						"Image File", WidgetPropertyCategory.Basic, new Path(
+								"bmp_PC_Gray.bmp"), FILE_EXTENSIONS));
+				model[11].addProperty(new FilePathProperty(PROP_IMAGE_FILE,
+						"Image File", WidgetPropertyCategory.Basic, new Path(
+								"bmp_PC_Gray.bmp"), FILE_EXTENSIONS));
+				model[12].addProperty(new FilePathProperty(PROP_IMAGE_FILE,
 						"Image File", WidgetPropertyCategory.Basic, new Path(
 								"bmp_PC_Gray.bmp"), FILE_EXTENSIONS));
 			}
@@ -142,159 +178,86 @@ public class TOPOEdit extends OPIEditor {
 		// model[i].setX(x);
 		// model[i].setY(y);
 		// }
-
+		String ip = "192.168.0.1-192.168.0.152,192.168.0.1-192.168.0.156";
+		
+		
 		String t = "0-1,0-2,0-3,1-4,1-5,1-6,2-7,2-8,2-9,3-10,3-11,3-12";
+		//String t = "00-01,00-02,00-03,02-04,02-05,02-06,02-07,02-08,02-9,03-10,03-1l,03-12";
 		String test[] = t.split(",");
 		for (int i = 0; i < test.length; i++) {
 			testList.add(test[i]);
 		}
 
+		//画头部
+		red.DrawHead();		
+		// 画部件
+		for (int i = 0; i < model.length; i++) {//model[i].getWUID() 6.model[i].getName()  把getWUID改为设备编号
+			red.Drawnode(482, 84, 122, 122, i, i,
+					"rectangle", "#FFCC00", "#000000");
+		}
+		
 		Iterator it = testList.iterator();
 		String mStr = "";
 		String spitStr[];
 		for (int i = 0; i < test.length; i++) {
 			mStr = (String) it.next();
 			spitStr = mStr.split("-");
+			//System.out.println(spitStr[0] + "-" + spitStr[1]);
+			// 设置连接模型
 			cModels[i].connect(model[Integer.parseInt(spitStr[0])], "BOTTOM",
 					model[Integer.parseInt(spitStr[1])], "TOP");
 			// addEdge(spitStr[0],spitStr[1]);//设置模型坐标
+			
+			red.Drawedge(spitStr[0], spitStr[1], "备注信息", "#000000", "standard");
+			
 		}
-		load();
+
+//		// 画边
+//		for (int j = 0; j < cModels.length - 1; j++) {
+//			red.Drawedge(cModels[j].getSource().getWUID(), cModels[j].getTarget()
+//					.getWUID(), "备注信息", "#000000", "standard");
+//		}
+		//生成结束
+		red.DrawOver();
+		
+		red.readNode();
+		red.readEdge();
+		red.load();// 生成opi
 	}
 
-	/**
-	 * Task:step 1.生成gml文件 2.调用yEd API 生成想要的布局方式 3.把gml转换为OPI格式
-	 */
-	private void load() {
-		// TODO Auto-generated method stub
-		System.out.println("加载");
-		DrawHead();
-		QueryBusinessObjectDef();
-		DrawOver();
-	}
+//	/**
+//	 * Task:step 1.生成gml文件 2.调用yEd API 生成想要的布局方式 3.把gml转换为OPI格式
+//	 */
+//	public void load() {
+//		System.out.println("加载");
+//		DrawHead();
+//		QueryBusinessObjectDef();
+//		DrawOver();
+//	}
 
-	/**
-	 * 查询所有业务对象 1.查询所有模型。 2.获取节点与边的信息
-	 */
-	private void QueryBusinessObjectDef() {
-		// 画节点
-		for (int i = 0; i < model.length; i++) {
-			Drawnode(482, 84, 122, 122, model[i].getWUID(), model[i].getName(),
-					"rectangle", "#FFCC00", "#000000");
-		}
-		// 画边
-		for (int j = 0; j < cModels.length - 1; j++) {
-			Drawedge(cModels[j].getSource().getWUID(), cModels[j].getTarget()
-					.getWUID(), "备注信息", "#000000", "standard");
-		}
-	}
+//	/**
+//	 * 查询所有业务对象 1.查询所有模型。 2.获取节点与边的信息
+//	 */
+//	private void QueryBusinessObjectDef() {
+//		// 画节点
+//		for (int i = 0; i < model.length; i++) {//6.model[i].getName()  把getWUID改为设备编号
+//			Drawnode(482, 84, 122, 122, model[i].getWUID(), model[i].getWUID(),
+//					"rectangle", "#FFCC00", "#000000");
+//		}
+//		// 画边
+//		for (int j = 0; j < cModels.length - 1; j++) {
+//			Drawedge(cModels[j].getSource().getWUID(), cModels[j].getTarget()
+//					.getWUID(), "备注信息", "#000000", "standard");
+//		}
+//	}
 
-	/**
-	 * 生成结束
-	 */
-	private void DrawOver() {
-		strContext.append("] \n");
-		// 设置一个默认文件夹路径
-		String path = "C:\\拓扑图\\";
-		File fileUpdate = new File(path);
-		if (fileUpdate.exists() == false) {
-			fileUpdate.mkdirs();
-		} else {
-			System.out.println("文件路径存在:" + path);
-		}
-		// 创建gml文件
-		File f = new File(path + "css.gml");
-		OutputStream out = null;
-		try {
-			out = new FileOutputStream(f);
-			byte b[] = strContext.toString().getBytes();
-			for (int i = 0; i < b.length; i++) {
-				out.write(b[i]);
-			}
-			out.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
 
-	/**
-	 * 生成头部
-	 */
-	private void DrawHead() {
-		strContext.append("Creator	\"yFiles\" ");
-		strContext.append("\nVersion	\"2.8\" ");
-		strContext.append("\ngraph ");
-		strContext.append("\n[\n\thierarchic	1");
-		strContext.append("\n\tlabel	\"\"");
-		strContext.append("\n\tdirected	1 \n");
-	}
 
-	/**
-	 * Task:生成节点
-	 * 
-	 * @param x
-	 * @param y
-	 * @param w
-	 * @param h
-	 * @param id
-	 * @param name
-	 * @param type
-	 * @param color
-	 * @param outline
-	 */
-	public void Drawnode(int x, int y, int w, int h, String id, String name,
-			String type, String color, String outline) {
-		strContext.append("\t node\n \t[\n");
-		strContext.append("\t\t id \"" + id + "\"\n");
-		strContext.append("\t\t label \"" + name + "\"\n");
-		strContext.append("\t\t graphics\n \t\t[ \n");
-		strContext.append("\t\t\t x " + x + "\n");
-		strContext.append("\t\t\t y " + y + "\n");
-		strContext.append("\t\t\t w " + w + "\n");
-		strContext.append("\t\t\t h " + h + "\n");
-		strContext.append("\t\t\t type \"" + type + "\"\n");
-		strContext.append("\t\t\t fill \"" + color + "\"\n");
-		strContext.append("\t\t\t outline \"" + outline + "\"\n");
-		strContext.append("\t\t ] \n");
-		strContext.append("\t\t LabelGraphics\n \t\t [ \n");
-		strContext.append("\t\t\t text \"" + name + "\"\n");
-		strContext.append("\t\t\t fontSize 12\n");
-		strContext.append("\t\t\t fontName \"Dialog\"\n");
-		strContext.append("\t\t\t anchor \"c\"\n");
-		strContext.append("\t\t ] \n");
-		strContext.append("\t ] \n");
-	}
 
-	/**
-	 * Task:生成边
-	 * 
-	 * @param source
-	 * @param target
-	 * @param name
-	 * @param color
-	 * @param targetArrow
-	 */
-	public void Drawedge(String source, String target, String name,
-			String color, String targetArrow) {
-		// name="";
-		strContext.append("\t edge\n \t[\n");
-		strContext.append("\t\t source \"" + source + "\"\n");
-		strContext.append("\t\t target \"" + target + "\"\n");
-		strContext.append("\t\t label \"" + name + "\"\n");
-		strContext.append("\t\t graphics\n \t\t[ \n");
-		strContext.append("\t\t\t fill \"" + color + "\"\n");
-		strContext.append("\t\t\t targetArrow \"" + targetArrow + "\"\n");
-		strContext.append("\t\t ] \n");
-		strContext.append("\t\t LabelGraphics\n \t\t [ \n");
-		strContext.append("\t\t\t text \"" + name + "\"\n");
-		strContext.append("\t\t\t fontSize 12\n");
-		strContext.append("\t\t\t fontName \"Dialog\"\n");
-		strContext.append("\t\t\t model \"six_pos\"\n");
-		strContext.append("\t\t\t position \"tail\"\n");
-		strContext.append("\t\t ] \n");
-		strContext.append("\t ] \n");
 
-	}
+
+
+
 
 	/**
 	 * 判断 生成 字符长度大小
