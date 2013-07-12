8bcef8c90c1f338949d8953e22f48ad7aa5daefc
haiming.wang <haiming.wang@haimingwang-PC>
2013-07-12 10:28:02 +0800

    公共变量

---
+++ siteview/com.siteview.css.topo/src/com/siteview/css/topo/common/TopoData.java
@@ -0,0 +1,12 @@
+package com.siteview.css.topo.common;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.siteview.snmp.pojo.Edge;
+
+public class TopoData {
+
+	public static boolean isInit = false;
+	public static List<Edge> edgeList = new ArrayList<Edge>();
+}
