83cc12af80917921127af19c3801e1cd0f22811b
haiming.wang <haiming.wang@haimingwang-PC>
2013-07-16 15:30:27 +0800

    添加设备信息

--- siteview/com.siteview.css.topo/src/com/siteview/css/topo/common/TopoData.java
+++ siteview/com.siteview.css.topo/src/com/siteview/css/topo/common/TopoData.java
@@ -1,12 +1,23 @@
 package com.siteview.css.topo.common;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.siteview.snmp.pojo.Edge;
+import com.siteview.snmp.pojo.IDBody;
 
 public class TopoData {
 
+	
 	public static boolean isInit = false;
+	/**
+	 * 边信息
+	 */
 	public static List<Edge> edgeList = new ArrayList<Edge>();
+	/**
+	 * 设备信息
+	 */
+	public static Map<String, IDBody> deviceList = new HashMap<String, IDBody>();
 }
