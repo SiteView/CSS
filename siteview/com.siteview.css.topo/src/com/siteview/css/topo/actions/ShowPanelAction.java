d496e277db604b756401e5e2af392f47d68bd2f4
haiming.wang <haiming.wang@haimingwang-PC>
2013-08-08 13:21:31 +0800

    添加css模型右键菜单打开设备详细信息和面板图的代码

---
+++ siteview/com.siteview.css.topo/src/com/siteview/css/topo/actions/ShowPanelAction.java
@@ -0,0 +1,13 @@
+package com.siteview.css.topo.actions;
+
+import org.csstudio.opibuilder.actions.AbstractWidgetTargetAction;
+import org.eclipse.jface.action.IAction;
+
+public class ShowPanelAction extends AbstractWidgetTargetAction {
+
+	@Override
+	public void run(IAction action) {
+
+	}
+
+}
