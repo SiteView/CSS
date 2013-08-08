bbaa3443d88e521212ab1aee298fc634e22bd855
yuzhenbiao <zhenbiao.yu@dragonflow.com>
2013-08-08 10:21:06 +0800

    路由器面板图开发用到的一些实体类

---
+++ siteview/com.siteview.css.topo/src/com/siteview/css/topo/pojo/Point.java
@@ -0,0 +1,31 @@
+package com.siteview.css.topo.pojo;
+
+public class Point {
+	public long x;
+	public long y;
+
+	public Point() {
+	}
+
+	public Point(long x, long y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public long getX() {
+		return x;
+	}
+
+	public void setX(long x) {
+		this.x = x;
+	}
+
+	public long getY() {
+		return y;
+	}
+
+	public void setY(long y) {
+		this.y = y;
+	}
+
+}
