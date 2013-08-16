a76aa214559b4b5ed4243b0507306518161efc84
haiming.wang <haiming.wang@haimingwang-PC>
2013-08-16 09:22:40 +0800

    修改BUG

--- siteview/com.siteview.css.topo/src/com/siteview/css/topo/factory/ImageFactory.java
+++ siteview/com.siteview.css.topo/src/com/siteview/css/topo/factory/ImageFactory.java
@@ -16,14 +16,14 @@ public class ImageFactory {
 	public static Image bumbImg = null;
 	public static synchronized Image getPcBlueImage(){
 		if(pcImage == null){
-		    URL url = bundle.getEntry("icons/png_PC_Blue.png");
+		    URL url = bundle.getEntry("icons/PC_Blue.png");
 		    pcImage = ImageDescriptor.createFromURL(url).createImage();
 		}
 		return pcImage;
 	}
 	public static synchronized Image getRouterBlueImage(){
 		if(routerImage == null){
-			 URL url = bundle.getEntry("icons/png_Router_Blue.png");
+			 URL url = bundle.getEntry("icons/Router_Blue.png");
 			 routerImage = ImageDescriptor.createFromURL(url).createImage();
 		}
 		return routerImage;
@@ -37,14 +37,14 @@ public class ImageFactory {
 	}
 	public static synchronized Image getSwitchRouterBlueImage(){
 		if(switchRouterImg == null){
-			URL url = bundle.getEntry("icons/png_SwitchRouter_Blue.png");
+			URL url = bundle.getEntry("icons/SwitchRouter_Blue.png");
 			switchRouterImg = ImageDescriptor.createFromURL(url).createImage();
 		}
 		return switchRouterImg;
 	}
 	public static synchronized Image getDumbImage(){
 		if(bumbImg == null){
-			URL url = bundle.getEntry("icons/png_Hub.png");
+			URL url = bundle.getEntry("icons/Hub.png");
 			bumbImg = ImageDescriptor.createFromURL(url).createImage();
 		}
 		return bumbImg;
